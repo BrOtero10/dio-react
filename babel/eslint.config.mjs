30e88d242c775dac58322686cb2d8489e565ea75
BrOtero <bruno2004.aotero@gmail.com>
2024-09-19 19:25:52 -0300

    feat: instalação e configuração do prettier

--- babel/eslint.config.mjs
+++ babel/eslint.config.mjs
@@ -8,4 +8,16 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // Desabilita a exigência de React no escopo para JSX a partir do React 17+
+      'react/react-in-jsx-scope': 'off',
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react",
+      "plugin:react/recommended",
+      "plugin:prettier/recommended",
+    ]
+  }
 ];
